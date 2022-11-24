f70e3ed319300a58caea240d54a8eaafd1fc70db
P Luong <p-luong@live.co.uk>
2022-11-24 17:24:03 +0000

    created query to execute the removeBook mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -40,3 +40,15 @@ export const SAVE_BOOK = gql`
     }
   }
 `;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks
+    }
+  }
+`;
