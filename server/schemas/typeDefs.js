96acb5ca3a07858a50250f58cb7b1d398a254d4c
P Luong <p-luong@live.co.uk>
2022-11-24 15:05:45 +0000

    define user and book type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,26 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
 
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+    title: String
+}
+
+type Query {
+
+}
 `;
 
 module.exports = typeDefs;
