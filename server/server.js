f4acdcf7507f67c725002b57688eb284dbd2e482
P Luong <p-luong@live.co.uk>
2022-11-24 14:50:52 +0000

    commented out code in server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+//const path = require('path');
 const db = require('./config/connection');
 
 // importing the ApolloServer class
@@ -20,9 +20,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+//if (process.env.NODE_ENV === 'production') {
+//  app.use(express.static(path.join(__dirname, '../client/build')));
+//}
 
 // Creating a new instance of an ApolloServer with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
