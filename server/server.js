d10b5da03d6ed5c069617d6bc7030345278cb8d7
P Luong <p-luong@live.co.uk>
2022-11-24 14:49:45 +0000

    called startApolloServer func in server.js

--- server/server.js
+++ server/server.js
@@ -31,5 +31,10 @@ const startApolloServer = async (typeDefs, resolvers) => {
 }
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
+
+startApolloServer(typeDefs, resolvers);
