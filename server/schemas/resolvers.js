d945e10f09ace013f4bd77e7ae4a8edb2252712f
P Luong <p-luong@live.co.uk>
2022-11-24 16:36:46 +0000

    added type mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,8 +5,12 @@ const resolvers = {
         me: async (parent, args) => {
             return await User.findById(args.id).populate('books');
         }
+    },
+    Mutation: {
+        login: async (parent, {  }) => {
+            
+        }
     }
-
 }
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,7 +29,10 @@ type Query {
 }
 
 type Mutation {
-    login
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String], description: String, title: String!, bookId: String!, image: String, link: String!): User
+    removeBook(bookId: String!): User
 }
 `;
 
